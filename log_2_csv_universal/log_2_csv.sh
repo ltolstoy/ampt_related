70e1a7d8e468bf931e4d2f5c6defde58f3c46feb
ltolstoy <leontol@gmail.com>
2017-08-17 13:09:08 -0600

    Delete log_2_csv.sh

--- log_2_csv_universal/log_2_csv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script to wrap /media/win_share/scripts/log_2_csv_universal/log_2_csv_universal_vX.py
-# Run: log_2_csv.sh /media/win_EON/data_log/nano/150404/20150404_b1.log
-P="/media/win_share/scripts/log_2_csv_universal/log_2_csv_universal_v10.py"
-$P -i $1
